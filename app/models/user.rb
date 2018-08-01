bfc4a24ea0abd43c52d7f3bb1d4b637bec5bed11
EC2 Default User <ec2-user@ip-10-0-0-109.ec2.internal>
2018-08-01 16:14:04 +0000

    Validation is in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
-class User < ApplicationRecord
-    
+class User < ActiveRecord::Base
+    #Username validation
+    validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
+    #Email validation
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 end
